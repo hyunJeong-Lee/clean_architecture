8942eeb35a53f35ca5fe0e206a015a2aef286ca4
이현정 <leehyun47580@gmail.com>
2024-06-25 00:13:06 +0900

    UI Layer 의존성 설정
    [Domain Layer 의존성 추가]
    Domain Layer 에 정의된 유스케이스를 사용해 사용자와의 상호작용을 기반으로 비즈니스 로직을 실행.
    
    [Data Layer 의존성 추가]
    로컬 데이터베이스나 preference 에 직접 접근하는 등 특정 데이터의 처리 작업을 위해 Data Layer 의 기능 사용.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Domain, Data Layer 에 대한 의존성 추가.
+    implementation(project(":domain"))
+    implementation(project(":data"))
 }

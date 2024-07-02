f6c39c5cdc6601aec538a7f778bec37aa78d3e26
이현정 <leehyun47580@gmail.com>
2024-07-02 11:07:20 +0900

    [Data] Retrofit 종속성 추가
    - 서버 통신을 위해 Retrofit 라이브러리 종속성 추가.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -54,4 +54,9 @@ dependencies {
 
     //gson
     implementation("com.google.code.gson:gson:2.9.0")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
 }
